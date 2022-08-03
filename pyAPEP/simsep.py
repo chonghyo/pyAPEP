f2ea4d742621077cedfd384d2c57245496987f68
chonghyo <86150567+chonghyo@users.noreply.github.com>
2022-08-03 22:56:26 +0900

    Update simsep.py

--- pyAPEP/simsep.py
+++ pyAPEP/simsep.py
@@ -1,6 +1,6 @@
 """
 ====================================
- :mod:`pyAPEP.isofit` 
+Real PSA simulation module (:py:mod:`pyAPEP.simsep`)
 ====================================
 
 .. moduleauthor:: jjh <hyo156@kitech.re.kr>
@@ -22,18 +22,21 @@ Functions Functions Functions Functions
 Functions Functions Functions Functions 
  * [2022/08/02] - test
 """
-
-
-# %% Import packages
-#from numpy.lib.function_base import _parse_input_dimensions
-
-#import numpy as np
-#from scipy.integrate import odeint
-#import matplotlib.pyplot as plt
-#import time
-#from scipy.interpolate import interp1d
-#from scipy.integrate import solve_ivp
-
+class exam1:
+    """문서화 테스트를 위한 임시 클래스
+    """
+    def __init__(self, xx: int):
+        """ 클래스 생성자
+        :param xx: 생성자 파라미터
+        >>> xx = exam1(10)
+        """
+        self.xx = xx
+    def print_value(self):
+        """ 멤버변수 xx를 출력하는 메소드
+        >>> exam1(10).print_value()
+        10
+        """
+        print(self.xx)
 
 # %% Global varaiebls
 R_gas = 8.3145      # 8.3145 J/mol/K
