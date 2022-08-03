aa47fc2a3aa6c40ed1f2a899a9cfac763c0576dd
unknown <hyo156@naver.com>
2022-08-03 09:01:22 +0900

    second

---
+++ pyAPEP/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Aug  1 16:04:29 2022
+
+@author: PSID-Notebook15
+"""
+
---
+++ pyAPEP/simide.py
@@ -0,0 +1,39 @@
+"""
+====================================
+ :mod:`pyAPEP.simide`
+====================================
+.. moduleauthor:: jjh <hyo156@kitech.re.kr>
+.. note:: 따라하세요
+
+Theory
+======
+Theory Theory Theory Theory Theory Theory 
+Theory Theory Theory Theory Theory Theory 
+
+Tutorials
+=========
+Tutorials Tutorials Tutorials Tutorials
+Tutorials Tutorials Tutorials Tutorials
+
+Functions
+=========
+Functions Functions Functions Functions 
+Functions Functions Functions Functions 
+
+ * [2022/08/02] - test
+"""
+class exam1:
+    """문서화 테스트를 위한 임시 클래스
+    """
+    def __init__(self, xx: int):
+        """ 클래스 생성자
+        :param xx: 생성자 파라미터
+        >>> xx = exam1(10)
+        """
+        self.xx = xx
+    def print_value(self):
+        """ 멤버변수 xx를 출력하는 메소드
+        >>> exam1(10).print_value()
+        10
+        """
+        print(self.xx)
